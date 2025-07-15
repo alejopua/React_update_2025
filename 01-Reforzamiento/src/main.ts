86e4e8d2d7b0db08e29d27866ace6f09dff5ae39
alejopua <alejopua@gmail.com>
2025-07-14 21:56:06 -0500

    feat: update main.ts to include commented imports and log heroes by owner

--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -1,4 +1,13 @@
 import './style.css'
+// import './bases/01-const-let'
+// import './bases/02-templete-string'
+// import './bases/03-object-literal'
+// import './bases/04-arrays'
+// import './bases/05-functions'
+// import './bases/06-obj-destructuring'
+// import './bases/07-array-destructuring'
+import { getHeroesByOwner } from './bases/08-imp-exp'
+import { Owner } from './bases/data/heroes.data';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -7,4 +16,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </h1>
   </div>
 `
+
+console.log(getHeroesByOwner(Owner.DC));
 
