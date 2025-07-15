da0c8a18f8516820162e457de68a81426996fd75
alejopua <alejopua@gmail.com>
2025-07-14 21:03:15 -0500

    feat: add heroes data with Hero interface and Owner enum

---
+++ 01-Reforzamiento/src/bases/data/heroes.data.ts
@@ -0,0 +1,46 @@
+// Interfaces --> Definen la estructura de un objeto, especificando sus propiedades y tipos.
+
+// Enums --> Permiten definir un conjunto de constantes con nombre, facilitando la legibilidad y el mantenimiento del código.
+
+// types --> Son alias para tipos de datos, permitiendo crear tipos personalizados y más complejos a partir de tipos primitivos o combinaciones de ellos.
+
+interface Hero {
+  id: number;
+  name: string;
+  owner: Owner;
+}
+
+// type Owner = 'DC' | 'Marvel';
+
+enum Owner {
+  DC = 'DC',
+  Marvel = 'Marvel',
+}
+
+const heroes: Hero[] = [
+  {
+    id: 1,
+    name: 'Batman',
+    owner: Owner.DC
+  },
+  {
+    id: 2,
+    name: 'Spiderman',
+    owner: Owner.Marvel,
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    owner: Owner.DC,
+  },
+  {
+    id: 4,
+    name: 'Flash',
+    owner: Owner.DC,
+  },
+  {
+    id: 5,
+    name: 'Wolverine',
+    owner: Owner.Marvel,
+  },
+];
