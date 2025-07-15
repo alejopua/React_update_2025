8a31aeec0786857c84d0148665d6bbff938ff9b3
alejopua <alejopua@gmail.com>
2025-07-14 20:16:28 -0500

    feat: add useState function example demonstrating array destructuring

--- 01-Reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-Reforzamiento/src/bases/07-array-destructuring.ts
@@ -9,3 +9,12 @@ const returnArrayFn = () => ['abc', 123] as const;
 const [letters, numbers] = returnArrayFn();
 
 console.log(numbers + 100);
+
+// Exercise
+const useState = (name: string) => {
+    return [name, (newName: string) => console.log(newName)] as const
+}
+
+const [name, setName] = useState('Goku');
+console.log({name});
+setName('Pachito');
